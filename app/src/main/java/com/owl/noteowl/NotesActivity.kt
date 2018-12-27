d313c2072f5b5237db5721557f89483c7eb646ee
Rishab Jaiswal <shivam.jaiswal175@gmail.com>
2018-12-27 15:57:57 +0530

    Added Notes screen

---
+++ app/src/main/java/com/owl/noteowl/NotesActivity.kt
@@ -0,0 +1,13 @@
+package com.owl.noteowl
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class NotesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notes)
+    }
+
+}
