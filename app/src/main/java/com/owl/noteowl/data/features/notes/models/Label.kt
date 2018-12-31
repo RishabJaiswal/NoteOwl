8f7d5eefaa0ac56ad08fcc88e79077426ae2f5b5
Rishab Jaiswal <shivam.jaiswal175@gmail.com>
2018-12-31 16:07:38 +0530

    Add Realm library

--- app/src/main/java/com/owl/noteowl/data/features/notes/models/Label.kt
+++ app/src/main/java/com/owl/noteowl/data/features/notes/models/Label.kt
@@ -1,8 +1,11 @@
 package com.owl.noteowl.data.features.notes.models
 
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
 import java.util.*
 
-class Label {
+class Label : RealmObject() {
+    @PrimaryKey
     var title: String = ""
     var createdAt: Date = Date()
 }
--- app/src/main/java/com/owl/noteowl/data/features/notes/models/Note.kt
+++ app/src/main/java/com/owl/noteowl/data/features/notes/models/Note.kt
@@ -1,11 +1,12 @@
 package com.owl.noteowl.data.features.notes.models
 
+import io.realm.RealmObject
 import java.util.*
 
-class Note {
+class Note : RealmObject() {
+    var createdAt: Date = Date()
     var title: String = ""
     var text: String = ""
-    var createdAt: Date = Date()
     var labels: List<Label> = emptyList()
     var isLiked: Boolean = false
 }
