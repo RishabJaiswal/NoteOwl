59b865c35ecf3251a3bcc4905a4598f1d136c654
Rishab Jaiswal <shivam.jaiswal175@gmail.com>
2018-12-31 15:54:59 +0530

    Added blankslate for home Added Android Core KTX

--- app/src/main/java/com/owl/noteowl/features/home/HomeActivity.kt
+++ app/src/main/java/com/owl/noteowl/features/home/HomeActivity.kt
@@ -3,6 +3,8 @@ package com.owl.noteowl.features.home
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.owl.noteowl.R
+import com.owl.noteowl.extensions.gone
+import com.owl.noteowl.extensions.visible
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
@@ -10,6 +12,14 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        rv_notes.adapter = NotesAdapter(this)
+        //showing blankslate
+        //todo: change later
+        val notesSize = 1
+        if (notesSize == 0) {
+            blank_slate_home.visible()
+        } else {
+            blank_slate_home.gone()
+            rv_notes.adapter = NotesAdapter(this)
+        }
     }
 }
---
+++ app/src/main/java/com/owl/noteowl/features/home/NotesViewModel.kt
@@ -0,0 +1,7 @@
+package com.owl.noteowl.features.home
+
+import androidx.lifecycle.ViewModel
+
+class NotesViewModel : ViewModel() {
+
+}
