0b4cc5e80aa96e528eb7cc8936d58a4f708a07b2
Rishab Jaiswal <shivam.jaiswal175@gmail.com>
2019-01-02 14:48:05 +0530

    Created screen to add note

---
+++ app/src/main/java/com/owl/noteowl/features/addNote/AddNoteActivity.kt
@@ -0,0 +1,12 @@
+package com.owl.noteowl.features.addNote
+
+import android.app.Activity
+import android.os.Bundle
+import com.owl.noteowl.R
+
+class AddNoteActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.add_note)
+    }
+}
--- app/src/main/java/com/owl/noteowl/features/home/HomeActivity.kt
+++ app/src/main/java/com/owl/noteowl/features/home/HomeActivity.kt
@@ -1,25 +1,39 @@
 package com.owl.noteowl.features.home
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.owl.noteowl.R
 import com.owl.noteowl.extensions.gone
 import com.owl.noteowl.extensions.visible
+import com.owl.noteowl.features.addNote.AddNoteActivity
 import kotlinx.android.synthetic.main.activity_home.*
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), View.OnClickListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
         //showing blankslate
         //todo: change later
-        val notesSize = 1
+        val notesSize = 0
         if (notesSize == 0) {
             blank_slate_home.visible()
         } else {
             blank_slate_home.gone()
             rv_notes.adapter = NotesAdapter(this)
+        }
+        btn_add_note.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            //add note
+            R.id.btn_add_note -> {
+                startActivity(Intent(this, AddNoteActivity::class.java))
+            }
         }
     }
 }
