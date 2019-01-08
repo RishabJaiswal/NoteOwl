8fbd5916fbcd733e2b8e6fe74f181e5195b3b89b
Rishab Jaiswal <shivam.jaiswal175@gmail.com>
2019-01-08 17:06:32 +0530

    Added binding adpater to set margins and changed how margin of item in recycler view is set

--- app/src/main/java/com/owl/noteowl/utils/ContextUtility.kt
+++ app/src/main/java/com/owl/noteowl/utils/ContextUtility.kt
@@ -1,16 +1,35 @@
 package com.owl.noteowl.utils
 
 import android.content.Context
+import android.util.TypedValue
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.ColorUtils
 import com.owl.noteowl.R
 
 class ContextUtility(val context: Context) {
 
+    val resources by lazy { context.resources }
+    val POSITION by lazy { Constants.Position() }
+
     //getting bgColor for a given background bgColor
     fun getTextColor(bgColor: Int): Int {
         if (ColorUtils.calculateLuminance(bgColor) < 0.5)
             return ContextCompat.getColor(context, android.R.color.white)
         return ContextCompat.getColor(context, R.color.textSecondary)
+    }
+
+    fun getMargin(viewType: Int, firstMargin: Int, midMargin: Int, endMargin: Int): Float {
+        val margin = if (viewType == POSITION.FIRST) {
+            firstMargin
+        } else if (viewType == POSITION.MID) {
+            midMargin
+        } else {
+            endMargin
+        }
+        return convertDpToPx(margin.toFloat())
+    }
+
+    fun convertDpToPx(dp: Float): Float {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, resources.displayMetrics)
     }
 }
