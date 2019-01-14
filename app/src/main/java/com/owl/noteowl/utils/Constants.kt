30409129dc23b7473f15b6111dd1ed01e17e3d9f
Rishab Jaiswal <shivam.jaiswal175@gmail.com>
2019-01-14 16:57:46 +0530

    Constants for note status and note related keys

--- app/src/main/java/com/owl/noteowl/utils/Constants.kt
+++ app/src/main/java/com/owl/noteowl/utils/Constants.kt
@@ -7,4 +7,14 @@ class Constants {
         val MID = 1
         val LAST = 2
     }
+
+    class NoteStatus {
+        val NEW_EDIT = "new.edit"
+        val SAVED_EDIT = "saved.edit"
+        val SAVED = "saved"
+    }
+
+    class Note {
+        val KEY_ID = "note.id"
+    }
 }
