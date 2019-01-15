338cc1a3b083bb86887c0b2a28c52018770e5092
Rishab Jaiswal <shivam.jaiswal175@gmail.com>
2019-01-15 18:05:40 +0530

    Created image loader adapter

--- app/src/main/java/com/owl/noteowl/utils/ImageLoader.kt
+++ app/src/main/java/com/owl/noteowl/utils/ImageLoader.kt
@@ -1,4 +1,19 @@
 package com.owl.noteowl.utils
 
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.owl.noteowl.R
+import com.squareup.picasso.Picasso
+
 class ImageLoader {
+    companion object {
+        @JvmStatic
+        @BindingAdapter("bind:loadImage")
+        fun loadImage(imv: ImageView, imageUrl: String) {
+            Picasso.get()
+                .load(imageUrl)
+                .placeholder(R.drawable.ic_cross)
+                .into(imv)
+        }
+    }
 }
