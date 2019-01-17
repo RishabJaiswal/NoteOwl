55a7a952f6a3134adca941b5ffecc47390f8d284
Rishab Jaiswal <shivam.jaiswal175@gmail.com>
2019-01-17 17:12:32 +0530

    image urls in image Pojo

--- app/src/main/java/com/owl/noteowl/data/features/images/models/Image.kt
+++ app/src/main/java/com/owl/noteowl/data/features/images/models/Image.kt
@@ -1,4 +1,8 @@
 package com.owl.noteowl.data.features.images.models
 
+import com.google.gson.annotations.SerializedName
+
 class Image {
+    @SerializedName("urls")
+    var urls: ImageUrls? = null
 }
