796c43d7fa7357a74500257099355f76a581442d
Rishab Jaiswal <shivam.jaiswal175@gmail.com>
2019-01-17 17:12:03 +0530

    removed base URL

--- app/src/main/java/com/owl/noteowl/data/features/images/network/ImageApiManager.kt
+++ app/src/main/java/com/owl/noteowl/data/features/images/network/ImageApiManager.kt
@@ -5,7 +5,6 @@ import com.owl.noteowl.data.features.images.models.Image
 import retrofit2.Call
 
 class ImageApiManager {
-    val baseUrl = "https://api.unsplash.com/photos"
     val clientId = "066dcd18f78faa93ac74ec0ca034aff957d17e7768cacc50d4e04f2596ec453b"
 
     val imageApiService by lazy {
@@ -13,6 +12,6 @@ class ImageApiManager {
     }
 
     fun getImages(pageSize: Int, pageNo: Int): Call<List<Image>> {
-        return imageApiService.getImages(baseUrl, clientId, pageSize, pageNo)
+        return imageApiService.getImages(clientId, pageSize, pageNo)
     }
 }
