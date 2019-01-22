ada457986c27e9804c1dcb3989358b0f89f8c488
Rishab Jaiswal <shivam.jaiswal175@gmail.com>
2019-01-22 15:20:00 +0530

    added methods to get labels

--- app/src/main/java/com/owl/noteowl/data/features/notes/local/LabelDao.kt
+++ app/src/main/java/com/owl/noteowl/data/features/notes/local/LabelDao.kt
@@ -1,7 +1,10 @@
 package com.owl.noteowl.data.features.notes.local
 
+import androidx.lifecycle.LiveData
 import com.owl.noteowl.data.features.notes.models.Label
+import com.owl.noteowl.extensions.asLiveData
 import io.realm.Realm
+import io.realm.RealmResults
 
 class LabelDao(val realm: Realm = Realm.getDefaultInstance()) {
 
@@ -9,5 +12,14 @@ class LabelDao(val realm: Realm = Realm.getDefaultInstance()) {
         realm.executeTransaction {
             it.copyFromRealm(label)
         }
+    }
+
+    fun getLabels(): RealmResults<Label> {
+        return realm.where(Label::class.java)
+            .findAll()
+    }
+
+    fun getLabelsLive(): LiveData<RealmResults<Label>> {
+        return getLabels().asLiveData()
     }
 }
