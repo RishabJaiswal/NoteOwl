c43eab09c2d5cf6811b635e8d1663a7a79123001
Rishab Jaiswal <shivam.jaiswal175@gmail.com>
2019-02-01 17:34:49 +0530

    created realm list live data

--- app/src/main/java/com/owl/noteowl/extensions/RealmExtensions.kt
+++ app/src/main/java/com/owl/noteowl/extensions/RealmExtensions.kt
@@ -1,18 +1,27 @@
 package com.owl.noteowl.extensions
 
 import androidx.lifecycle.LiveData
+import com.owl.noteowl.utils.RealmListLiveData
 import com.owl.noteowl.utils.RealmObjectLiveData
 import com.owl.noteowl.utils.RealmResultsLiveData
 import io.realm.Realm
+import io.realm.RealmList
 import io.realm.RealmObject
 import io.realm.RealmResults
 
+//Realm object live data
 fun <T : RealmObject> T.asLiveData(): LiveData<T> {
     return RealmObjectLiveData(this)
 }
 
+//Realm results live data
 fun <T : RealmObject> RealmResults<T>.asLiveData(): LiveData<RealmResults<T>> {
     return RealmResultsLiveData<T>(this)
+}
+
+//Realm list live data
+fun <T : RealmObject> RealmList<T>.asLiveData(): LiveData<RealmList<T>> {
+    return RealmListLiveData<T>(this)
 }
 
 fun <T : RealmObject> T.asNonManagedRealmCopy(realm: Realm = Realm.getDefaultInstance()): T {
