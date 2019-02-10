74c89c5f4f1cf2b6a1d20a1d67f27d9275c33cb9
Rishab <shivam.jaiswal175@gmail.com>
2019-02-10 11:02:18 +0530

    Added label to add note on home filters

--- app/src/main/java/com/owl/noteowl/features/home/LabelsForFilterAdapter.kt
+++ app/src/main/java/com/owl/noteowl/features/home/LabelsForFilterAdapter.kt
@@ -5,17 +5,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.owl.noteowl.R
 import com.owl.noteowl.data.features.notes.models.Label
 import com.owl.noteowl.databinding.ItemLabelHomeFiltersBinding
+import com.owl.noteowl.utils.Constants
 
 class LabelsForFilterAdapter(
     val context: Context,
     val labels: List<Label>,
     val onLabelClicked: (label: Label?) -> Unit
 ) :
-    RecyclerView.Adapter<LabelsForFilterAdapter.LabelViewHolder>() {
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LabelViewHolder {
+    private val POSITION = Constants.Position()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        if (viewType == POSITION.FIRST) {
+            return AddNoteViewHolder(
+                LayoutInflater.from(context).inflate(
+                    R.layout.item_add_note_label_filters,
+                    null,
+                    false
+                )
+            )
+        }
         return LabelViewHolder(
             ItemLabelHomeFiltersBinding.inflate(
                 LayoutInflater.from(context), parent, false
@@ -24,13 +37,23 @@ class LabelsForFilterAdapter(
     }
 
     override fun getItemCount(): Int {
-        return labels.size
+        return labels.size + 1
     }
 
-    override fun onBindViewHolder(holder: LabelViewHolder, position: Int) {
-        holder.bind(labels[position])
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is LabelViewHolder) {
+            holder.bind(labels[position])
+        }
     }
 
+    override fun getItemViewType(position: Int): Int {
+        if (position == 0)
+            return POSITION.FIRST
+        return super.getItemViewType(position)
+    }
+
+
+    //label view holder
     inner class LabelViewHolder(private val binding: ItemLabelHomeFiltersBinding) :
         RecyclerView.ViewHolder(binding.root), View.OnClickListener {
 
@@ -48,6 +71,17 @@ class LabelsForFilterAdapter(
                 label = labels[adapterPosition]
             }
             onLabelClicked(label)
+        }
+    }
+
+    //add note viewholder
+    inner class AddNoteViewHolder(val view: View) : RecyclerView.ViewHolder(view), View.OnClickListener {
+        init {
+            view.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            onLabelClicked(null)
         }
     }
 }
