e2514a839b192425cbaf20611f0e0b65abfe101d
Rishab Jaiswal <shivam.jaiswal175@gmail.com>
2019-03-14 13:56:34 +0530

    Added notwOwl splash screen and logo animation

--- app/src/main/java/com/owl/noteowl/features/BaseActivity.kt
+++ app/src/main/java/com/owl/noteowl/features/BaseActivity.kt
@@ -1,6 +1,6 @@
 package com.owl.noteowl.features
 
-import android.view.View
+import android.content.Intent
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,10 +10,14 @@ open class BaseActivity : AppCompatActivity() {
         super.onResume()
 
         //hiding status bar
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        //window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
     }
 
     protected fun showToast(stringRes: Int) {
         Toast.makeText(this, stringRes, Toast.LENGTH_SHORT).show()
+    }
+
+    protected fun <T> start(clazz: Class<T>) {
+        startActivity(Intent(this, clazz))
     }
 }
