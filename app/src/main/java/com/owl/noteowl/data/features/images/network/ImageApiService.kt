ea140492acb709169909d91a5474759b7e23be65
Rishab Jaiswal <shivam.jaiswal175@gmail.com>
2019-04-01 18:02:06 +0530

    Added api call to increment image downloads for unsplash

--- app/src/main/java/com/owl/noteowl/data/features/images/network/ImageApiService.kt
+++ app/src/main/java/com/owl/noteowl/data/features/images/network/ImageApiService.kt
@@ -2,8 +2,10 @@ package com.owl.noteowl.data.features.images.network
 
 import com.owl.noteowl.data.features.images.models.Image
 import com.owl.noteowl.data.features.images.models.ImageSearchResult
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ImageApiService {
@@ -23,4 +25,10 @@ interface ImageApiService {
         @Query("per_page") pageSize: Int,
         @Query("page") pageNo: Int
     ): Call<ImageSearchResult>
+
+    @GET("{download_url}/")
+    fun incrementDownload(
+        @Path("download_url") downloadUrl: String,
+        @Query("client_id") clientId: String
+    ): Call<ResponseBody>
 }
