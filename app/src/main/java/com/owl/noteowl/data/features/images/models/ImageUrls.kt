8500a13837b8e9b0186963f18f9f9c3e120be033
Rishab Jaiswal <shivam.jaiswal175@gmail.com>
2019-04-03 11:46:47 +0530

    Production fix:: crash fix when fetching images

--- app/src/main/java/com/owl/noteowl/data/features/images/models/ImageUrls.kt
+++ app/src/main/java/com/owl/noteowl/data/features/images/models/ImageUrls.kt
@@ -1,6 +1,11 @@
 package com.owl.noteowl.data.features.images.models
 
+import com.google.gson.annotations.SerializedName
+
 class ImageUrls {
+    @SerializedName("regular")
     var regular: String? = null
+
+    @SerializedName("thumb")
     var thumb: String? = null
 }
